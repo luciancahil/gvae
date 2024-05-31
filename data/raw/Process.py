c7a14e0c20c814dc5e3f743abb5901571d677406
luciancahil <royhe62@yahoo.ca>
2024-05-31 16:06:54 -0700

    Proces update

--- data/raw/Process.py
+++ data/raw/Process.py
@@ -1,11 +1,28 @@
 f = open("Train.csv", "r")
-out = open("Seeds.csv", "w")
 
+char_int_dict = dict()
 
+num_chars = 0
+max_size = 0
 line_num = 0
-next(f)
+threshold = 100
+count = 0
 for i, line in enumerate(f):
-    parts = line.split(",")
-    out.write(parts[0])
-    if(i >= 10):
-        break
+    max_size = max(max_size, len(line))
+
+    if(len(line) < threshold):
+        count += 1
+
+    for c in line:
+        if c not in char_int_dict and c != '\n':
+            char_int_dict[c] = num_chars
+            num_chars += 1
+    
+    if(i % 500 == 0):
+        print(i)
+
+
+print(char_int_dict)
+print("Largest: " + str(max_size))
+print("Count: " + str(count))
+print(float(count) / 71634)
